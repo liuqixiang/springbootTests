f156ddb163c550ec22beab254e2b18447b74d1d9
liuqixiang <654737000@qq.com>
2018-06-14 19:27:25 +0800

    springboot的helloword

---
+++ 01-study-hello/src/main/java/com/boot/study/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.boot.study.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public String hello() {
+		return "hello world";
+	}
+
+	@GetMapping("/map")
+	public Map<String, Object> map() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "小威老师");
+		map.put("age", 18);
+
+		return map;
+	}
+}
