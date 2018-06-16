6cebc7bb471f41d221afe35468a31d70d876ab67
liuqixiang <654737000@qq.com>
2018-06-16 09:15:29 +0800

    06 spring boot 日志slf4j

---
+++ 06-study-log/src/main/java/com/boot/study/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.boot.study.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	private static final Logger log=LoggerFactory.getLogger(HelloController.class);
+	@GetMapping
+	public String hello() {
+		log.debug("debug");
+		log.info("info");
+		log.error("error");
+		return "hello world";
+	}
+
+	
+}
